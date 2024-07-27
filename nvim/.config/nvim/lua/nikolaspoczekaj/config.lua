db09843bb1725e235309f9851710eb6bb13782fe
nikolaspoczekaj <nikolas.poczekaj@gmail.com>
2024-07-27 16:36:13 +0200

    sassasa

--- nvim/.config/nvim/lua/nikolaspoczekaj/config.lua
+++ nvim/.config/nvim/lua/nikolaspoczekaj/config.lua
@@ -3,7 +3,18 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
 vim.keymap.set("i", "jj", "<Esc>")
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes" 
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
 
 vim.opt.clipboard:append { 'unnamedplus' }
 
