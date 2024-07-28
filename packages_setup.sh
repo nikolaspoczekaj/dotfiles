a0eea659cd53642c0b876b23df85644946b0149e
nikolaspoczekaj <nikolas.poczekaj@gmail.com>
2024-07-28 03:28:20 +0200

    fertig

--- packages_setup.sh
+++ packages_setup.sh
@@ -7,7 +7,16 @@ sudo npm install node -y
 sudo apt install golang-go -y
 sudo apt install stow -y
 sudo apt install pavucontrol -y
+sudo apt install keepassxc -y
 mkdir ~/repos
 git clone --depth 1 https://github.com/ryanoasis/nerd-fonts ~/repos/nerdfonts
 ~/repos/nerdfonts/install.sh FiraCode
-
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt update -y
+stow nvim 
+sudo apt install neovim 
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+stow i3
+sudo apt install i3 picom polybar -y
+cp ./i3/.config/i3/pulseaudio-control /bin/pulseaudio-control 
+stow startup-scripts
