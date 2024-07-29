21c1ce0825c041b731efac411ba031e76b515605
nikolaspoczekaj <nikolas.poczekaj@gmail.com>
2024-07-29 13:29:15 +0200

    lazygit

--- setup.sh
+++ setup.sh
@@ -23,6 +23,12 @@ sudo apt install i3 picom polybar -y
 sudo cp ./i3/.config/i3/pulseaudio-control /bin/pulseaudio-control 
 stow startup-scripts
 
+#lazygit Installation
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
 #go Installation
 GO_VERSION="1.22.5"
 GO_TARBALL="go${GO_VERSION}.linux-amd64.tar.gz"
